410c33ee1214a9f73597f1ebced7ab82a19b0cb5
Y YANG <ioseryangyang@gmail.com>
2021-06-22 16:05:56 +0800

    init

---
+++ CarthageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthageDemo
+//
+//  Created by Yang on 2021/6/22.
+//
+import SwifterSwift
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.random
+    }
+
+
+}
+
